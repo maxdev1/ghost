ac6ffe3454b1c87368ce0b2992d905350e8a8bcc
Tommy Nguyen <remyabel@users.noreply.github.com>
2017-06-05 19:32:12 -0400

    Use cmake instead of zlib's configure

--- patches/ports/zlib/1.2.8/package.sh
+++ patches/ports/zlib/1.2.8/package.sh
@@ -1,14 +1,14 @@
 REMOTE_ARCHIVE="https://ghostkernel.org/repository/zlib/zlib-1.2.8.tar.gz"
 UNPACKED_DIR=zlib-1.2.8
 ARCHIVE=zlib-1.2.8.tar.gz
-REQUIRES_INSTALL_IN_SOURCE_DIR=1
+REQUIRES_INSTALL_IN_SOURCE_DIR=0
 
 port_unpack() {
 	tar -xf $ARCHIVE
 }
 
 port_install() {
-	CHOST=i686-ghost CC=$HOST-gcc ./configure --static --prefix=$PREFIX
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../../../../../i686-ghost-toolchain.cmake ../$UNPACKED_DIR
 	make
 	make DESTDIR=$SYSROOT install
 }
