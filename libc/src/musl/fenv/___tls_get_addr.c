00ab291359405465d54c8bb85a8b06fb151eb2b8
maxdev <lokoxe@gmail.com>
2020-01-18 20:38:53 +0100

    Remove sources from musl that cause duplicate symbols

---
+++ libc/src/musl/fenv/___tls_get_addr.c
@@ -0,0 +1,7 @@
+#warning What should this do?
+
+int ___tls_get_addr() {
+    klog("Not implemented: ___tls_get_addr");
+    for(;;);
+    return 0;
+}
---
+++ libc/src/musl/fenv/hwcap.c
@@ -0,0 +1,7 @@
+#warning What should this do?
+
+int __hwcap() {
+    klog("Not implemented: __hwcap");
+    for(;;);
+    return 0;
+}
