18cb38d0bca3126f582cee33f88d565a3976e45d
maxdev <lokoxe@gmail.com>
2020-01-19 16:52:23 +0100

    TLS test cases

--- applications/cocorun/libcoconut/src/coconut.cpp
+++ applications/cocorun/libcoconut/src/coconut.cpp
@@ -4,16 +4,24 @@
 #include "stdio.h"
 
 __thread int bar = 54;
+__thread int bee = 31;
 
 void coconutThrow()
 {
+	klog("library: errno from libc.so");
 	klog("errno %x = %i", &errno, errno);
 	errno = 321;
-	klog("%i", errno);
+	klog("-> %i == 321", errno);
 
+	klog("library: bar from local");
 	klog("bar %x = %i", &bar, bar);
 	bar = 25;
-	klog("%i == 25", bar);
+	klog("-> %i == 25", bar);
+
+	klog("library: bee from local");
+	klog("bee %x = %i", &bar, bar);
+	bar = 13;
+	klog("-> %i == 13", bee);
 
 	klog("throwing exception...");
 	throw 20;
