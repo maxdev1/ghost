7e8caa4714f0e05d2bae43aea871d0dc30ea3059
maxdev1 <lokoxe@gmail.com>
2020-02-12 16:31:01 +0100

    Remove debug code

--- kernel/src/kernel/memory/heap.cpp
+++ kernel/src/kernel/memory/heap.cpp
@@ -72,7 +72,7 @@ bool heapExpand()
 	chunkAllocatorExpand(&heapAllocator, G_CONST_KERNEL_HEAP_EXPAND_STEP);
 	heapEnd += G_CONST_KERNEL_HEAP_EXPAND_STEP;
 
-	logInfo("%! expanded to end %h (%ikb in use)", "kernheap", heapEnd, heapAmountInUse / 1024);
+	logDebug("%! expanded to end %h (%ikb in use)", "kernheap", heapEnd, heapAmountInUse / 1024);
 
 	return true;
 }
