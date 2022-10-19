3d9cf151b28dc32f61b9658813b35e58ccf750e7
maxdev1 <lokoxe@gmail.com>
2022-10-19 22:49:46 +0200

    Clean up proc-list

--- applications/proc/src/list/list.cpp
+++ applications/proc/src/list/list.cpp
@@ -20,16 +20,19 @@
 
 #include "list.hpp"
 
-#include <stdio.h>
 #include <ghost/kernquery.h>
+#include <ghost/user.h>
+#include <stdio.h>
 
 /**
  *
  */
-int countTasks() {
+int countTasks()
+{
 	g_kernquery_task_count_data out;
 	g_kernquery_status countstatus = g_kernquery(G_KERNQUERY_TASK_COUNT, (uint8_t*) &out);
-	if (countstatus != G_KERNQUERY_STATUS_SUCCESSFUL) {
+	if(countstatus != G_KERNQUERY_STATUS_SUCCESSFUL)
+	{
 		fprintf(stderr, "failed to query the kernel for the number of tasks (code %i)\n", countstatus);
 		return -1;
 	}
@@ -39,13 +42,15 @@ int countTasks() {
 /**
  *
  */
-bool getTaskIds(uint32_t initialBufSize, g_tid** out_ids, uint32_t* out_count) {
+bool getTaskIds(uint32_t initialBufSize, g_tid** out_ids, uint32_t* out_count)
+{
 
 	g_kernquery_task_list_data data;
 	data.id_buffer = new g_tid[initialBufSize];
 	data.id_buffer_size = initialBufSize;
 	g_kernquery_status liststatus = g_kernquery(G_KERNQUERY_TASK_LIST, (uint8_t*) &data);
-	if (liststatus != G_KERNQUERY_STATUS_SUCCESSFUL) {
+	if(liststatus != G_KERNQUERY_STATUS_SUCCESSFUL)
+	{
 		fprintf(stderr, "failed to query the kernel for the list of task ids (code %i)\n", liststatus);
 		return false;
 	}
@@ -58,13 +63,11 @@ bool getTaskIds(uint32_t initialBufSize, g_tid** out_ids, uint32_t* out_count) {
 /**
  *
  */
-int proc_list(int argc, char** argv) {
-
-	// get number of tasks in the system
+int proc_list(int argc, char** argv)
+{
 	int numTasks = countTasks();
-	if (numTasks == -1) {
+	if(numTasks == -1)
 		return -1;
-	}
 
 	// get list of task ids
 	int initialBufSize = numTasks + 10;
@@ -73,27 +76,27 @@ int proc_list(int argc, char** argv) {
 	getTaskIds(initialBufSize, &ids, &tasksFound);
 
 	// get detail information for each task
-	g_kernquery_task_get_data** infos = new g_kernquery_task_get_data*[tasksFound];
-	for (uint32_t i = 0; i < tasksFound; i++) {
-		g_kernquery_task_get_data* out = new g_kernquery_task_get_data;
-		out->id = ids[i];
-		g_kernquery_status getstatus = g_kernquery(G_KERNQUERY_TASK_GET_BY_ID, (uint8_t*) out);
-		if (getstatus != G_KERNQUERY_STATUS_SUCCESSFUL) {
-			fprintf(stderr, "failed to query the kernel for task %i (code %i)\n", i, getstatus);
+	g_kernquery_task_get_data* infos = new g_kernquery_task_get_data[tasksFound];
+	for(uint32_t i = 0; i < tasksFound; i++)
+	{
+		infos[i].id = ids[i];
+		g_kernquery_status getstatus = g_kernquery(G_KERNQUERY_TASK_GET_BY_ID, (uint8_t*) &infos[i]);
+		if(getstatus != G_KERNQUERY_STATUS_SUCCESSFUL)
+		{
+			printf("failed to query the kernel for task %i (code %i)\n", ids[i], getstatus);
 			continue;
 		}
-
-		infos[i] = out;
 	}
 
 	// print information
 	println("%5s %5s %6s %-20s %-38s", "TID", "PID", "MEM", "NAME", "PATH");
-	for (uint32_t pos = 0; pos < tasksFound; pos++) {
-		g_kernquery_task_get_data* entry = infos[pos];
+	for(uint32_t pos = 0; pos < tasksFound; pos++)
+	{
+		g_kernquery_task_get_data* entry = &infos[pos];
 
 		// print processes and vm86
-		if (entry->type == G_TASK_TYPE_DEFAULT || entry->type == G_TASK_TYPE_VM86) {
-
+		if(entry->id != -1 && (entry->type == G_TASK_TYPE_DEFAULT || entry->type == G_TASK_TYPE_VM86))
+		{
 			// print foot line
 			println("%5i %5i %6i %-20s %-38s", entry->id, entry->parent, entry->memory_used / 1024, entry->identifier, entry->source_path);
 		}
